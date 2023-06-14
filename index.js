88a33d350dac91a04a65ee1cb313fcd2926af9f2
Muxlisaxon <qochqorovamuxlisam@gmail.com>
2023-06-14 18:17:14 +0500

    send

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const close = document.getElementById("close");
-const open = document.getElementById("open");
-const modal = document.getElementById("modal");
 
-open.addEventListener("click", () => modal.classList.add("show-modal"));
-close.addEventListener("click", () => modal.classList.remove("show-modal"));
+function myFunction() { 
+  document.getElementById("myDialog").showModal(); 
+} 
 
-window.addEventListener("click", (e) => {
-  e.target === modal ? modal.classList.remove("show-modal") : false;
-});
+function removeFunction() {
+    const e = document.getElementById("myDialog");
+    e.remove();
+}
